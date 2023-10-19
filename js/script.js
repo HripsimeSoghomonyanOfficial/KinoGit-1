0c9c563527be91ae60267cb9631be2dce46c39ba
edgar-karapetyan <147828488+edgar-karapetyan@users.noreply.github.com>
2023-10-19 23:42:49 +0400

    ok

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll',()=>{
     if (scrollY>200) {
-        document.querySelector('.head-top-cont').style.cssText='box-shadow: 0 5px 15px -10px white; height: 40px;'
+        document.querySelector('.head-top-cont').style.cssText='height: 40px;'
     }else{
         document.querySelector('.head-top-cont').style.cssText=''
     }
