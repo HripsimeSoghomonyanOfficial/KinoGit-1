f4b5ddee3bf14f01e5070a9fbe252937a8f57e8f
edgar-karapetyan <147828488+edgar-karapetyan@users.noreply.github.com>
2023-10-19 23:53:40 +0400

    new styles and change media

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 window.addEventListener('scroll',()=>{
     if (scrollY>200) {
         document.querySelector('.head-top-cont').style.cssText='height: 40px;'
+        document.querySelector('.logo>h1').style.cssText='font-size:22px'
     }else{
         document.querySelector('.head-top-cont').style.cssText=''
+        document.querySelector('.logo>h1').style.cssText=''
     }
 })
