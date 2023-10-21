ffd74975595f4f2322865a3b0627980716234963
edgar-karapetyan <147828488+edgar-karapetyan@users.noreply.github.com>
2023-10-21 18:47:10 +0400

    change mobile styles

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-window.addEventListener('scroll',()=>{
-    if (scrollY>200) {
-        document.querySelector('.head-top-cont').style.cssText=' height: 40px;'
-    }else{
-        document.querySelector('.head-top-cont').style.cssText=''
-    }
-})
+// window.addEventListener('scroll',()=>{
+//     if (scrollY>200) {
+//         document.querySelector('.head-top-cont').style.cssText=' height: 40px;'
+//     }else{
+//         document.querySelector('.head-top-cont').style.cssText=''
+//     }
+// })
